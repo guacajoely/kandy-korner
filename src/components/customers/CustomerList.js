33452b019d2f02620600b07e05eb1142c5aa9d38
guacajoely <guacajoely@gmail.com>
2023-04-17 11:00:15 -0400

    fix warning in CustomerList module again..

--- src/components/customers/CustomerList.js
+++ src/components/customers/CustomerList.js
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
 import { Customer } from "./Customer.js"
 import "./Customers.css"
 
 export const CustomerList = () => {
 
     const [customers, setCustomers] = useState([])
-    const navigate = useNavigate()
 
     useEffect(() => {
         fetch('http://localhost:8088/users?isStaff=false')
